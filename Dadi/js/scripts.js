1edb76e32b58a8049a92e3b44ba3c4f3ddcf0455
gabrielebottari <gabriele.bottari1@gmail.com>
2023-12-06 16:43:40 +0100

    ADD dice game style end exercise

--- Dadi/js/scripts.js
+++ Dadi/js/scripts.js
@@ -4,23 +4,57 @@ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
-// Generare un numero random da 1 a 6
-const diceThrowPlayer = Math.floor(Math.random() * 6) + 1;
-const diceThrowComputer = Math.floor(Math.random() * 6) + 1;
+//NB lascio di proprosito le varie possibilità come commenti e tengo quella che esteticamente preferisco
 
-// Lancio dei dadi per il giocatore e il computer
-const player = diceThrowPlayer;
-const computer = diceThrowComputer;
+//const playground = document.querySelector('#playground');
 
-// Stampa i risultati
-console.log("Player's result:", player);
-console.log("CPU's result:", computer);
+const myButton = document.getElementById('play');
 
-// Stabilire il vincitore
-if (player > computer) {
-    console.log("Player win!");
-} else if (player < computer) {
-    console.log("CPU win!");
-} else {
-    console.log("Draw!");
-}
+myButton.addEventListener('click', function() {
+
+    // Generare un numero random da 1 a 6
+    const diceThrowPlayer = Math.floor(Math.random() * 6) + 1;
+    const diceThrowComputer = Math.floor(Math.random() * 6) + 1;
+
+    // Lancio dei dadi per il giocatore e il computer
+    const player = diceThrowPlayer;
+    const computer = diceThrowComputer;
+
+    // Stampa i risultati
+    console.log("Player's result:", player);
+    console.log("CPU's result:", computer);
+
+    //const result = document.createElement('div');
+
+    // Stabilire il vincitore
+    let = result;
+
+    if (player > computer) {
+        console.log("Player win!");
+        result = ("Player win!");
+        document.getElementById("result").className = "player";
+        //result.classList.add("text-center", "m-2", "fs-3", "text-warning");
+        //result.append("Player win!");
+
+    } else if (player < computer) {
+        console.log("CPU win!");
+        result = ("CPU win!");
+        document.getElementById("result").className = "cpu";
+        //result.classList.add("text-center", "m-2", "fs-3", "text-danger");
+        //result.append("CPU win!");
+
+    } else {
+        console.log("Draw!");
+        result = ("Draw!");
+        document.getElementById("result").className = "draw";
+        //result.classList.add("text-center", "m-2", "fs-3", "text-info");
+        //result.append("Draw!");
+    }
+
+    // Inserimento elemento nel container
+    // playground.append(result);
+
+    // Stampa il risultato nell'HTML
+    document.getElementById("result").innerHTML = result;
+
+})
