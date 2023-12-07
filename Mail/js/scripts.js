34eebdb7ae26aabb27a34a082ee32992f4de890d
gabrielebottari <gabriele.bottari1@gmail.com>
2023-12-07 10:03:45 +0100

    FIX mail with cicle and flag

--- Mail/js/scripts.js
+++ Mail/js/scripts.js
@@ -5,6 +5,7 @@ controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull'esito del controllo.
 */
 
+/*
 // Lista di chi può accedere
 const mailList = ["example1@email.com", "example2@email.com", "example3@email.com"];
 
@@ -26,4 +27,29 @@ myButton.addEventListener('click', function() {
         document.getElementById('result').innerHTML = ("La tua email non è autorizzata!");
     }
     
+})
+*/
+
+const mailList = ["example1@email.com", "example2@email.com", "example3@email.com"];
+
+const myButton = document.getElementById('login');
+
+myButton.addEventListener('click', function() {
+
+    // Ottieni il valore inserito dall'utente
+    const userMail = document.getElementById('mail').value;
+
+    // Inizializza una variabile per il risultato
+    let result = "La tua email non è autorizzata.";
+
+    // Verifica se l'email è nella lista di chi può accedere
+    for (let i = 0; i < mailList.length; i++) {
+        if (userMail == mailList[i]) {
+            result = "Accesso consentito. Benvenuto!";
+            break; // Esci dal ciclo se trovi un match
+        }
+    }
+
+    // Stampa il risultato nell'HTML
+    document.getElementById("result").innerText = result;
 })
